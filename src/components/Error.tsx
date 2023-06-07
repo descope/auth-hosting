99296037628b6b1ed53b632318ee988bce896913
Tal Aharoni <105210819+talaharoni@users.noreply.github.com>
2023-06-07 22:13:45 +0300

    Initial commit for the oidc-hosting app (#1)

---
+++ src/components/Error.tsx
@@ -0,0 +1,20 @@
+import '../App.css';
+import { useCopyToClipboard } from 'usehooks-ts'
+
+
+function Error() {
+    const [value, copy] = useCopyToClipboard()
+    const exampleText = "https://descope-oidc.com?projectId=PROJECT_ID&flow_id=FLOW_ID&demo=true"
+  
+    return (
+      <div className='page error fullscreen'>
+        <h1 className='title'>Hmmmmm</h1>
+        <p>Please make sure the URL is correctly formatted with the right <span>project id</span> and <span>flow id</span>.</p>
+        <p>Here's an example (for descope-oidc.com): </p>
+        <p className='example'><a href={exampleText}>{exampleText}</a></p>
+      </div>
+    )
+}
+
+
+export default Error
