3d769096efccf21fbb91f9e368c2cb359cfe3dee
talaharoni <tal@descope.com>
2023-06-22 20:51:09 +0300

    align to screen size

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
 import './App.css';
-import { AuthProvider, Descope } from "@descope/react-sdk";
+import { useState } from 'react'
+import { AuthProvider, Descope } from '@descope/react-sdk';
 import Error from './components/Error';
 
+const App = () => {
+	const urlParams = new URLSearchParams(window.location.search);
+	const projectId = urlParams.get('project') || '';
+	const flowId = urlParams.get('flow') || 'sign-up-or-in';
+	const debug = urlParams.get('debug') === 'true';
+    const [error, setError] = useState(false)
 
-function App() {
-  const urlParams = new URLSearchParams(window.location.search)
-  const projectId = urlParams.get("project") || ""; 
-  const flowId = urlParams.get("flow") || "sign-up-or-in";
-  const debug = urlParams.get("debug") === "true";
-
-  return (
-    <>
-      {projectId && flowId ? 
-        <>
-        <AuthProvider projectId={projectId}>
-          <Descope
-            flowId={flowId}
-            debug={debug}
-            onError={(e) => console.log('Could not logged in!')}
-          ></Descope>
-        </AuthProvider>
-        </>
-        :
-        <Error />
-      }
-    </>
-  )
-}
-
+	return (
+		<div className='page authflow'>
+			{projectId && flowId && !error? (
+                <div className='flow-shown'>
+                    <AuthProvider projectId={projectId}>
+                        <Descope
+                            flowId={flowId}
+                            debug={debug}
+                            onError={(e) => setError(true)}
+                        />
+                    </AuthProvider>
+                </div>
+			) : (
+				<Error />
+			)}
+		</div>
+	);
+};
 
 export default App;
--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,14 +1,27 @@
 import '../App.css';
+import { useCopyToClipboard } from 'usehooks-ts'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import CheckIcon from '@mui/icons-material/Check';
+
 
 function Error() {
-    const exampleText = "https://descope-oidc.com?project=PROJECT_ID&flow=FLOW_ID&debug=true"
-  
+    const [value, copy] = useCopyToClipboard()
+    const exampleText = `https://descope-oidc.com?project=PROJECT_ID&flow=FLOW_ID&debug=true`;
+    
     return (
       <div className='page error fullscreen'>
-        <h1 className='title'>Hmmmmm</h1>
-        <p>Please make sure the URL is correctly formatted with the right <span>project id</span> and <span>flow id</span>.</p>
-        <p>Here's an example (for descope-oidc.com): </p>
-        <p className='example'><a href={exampleText}>{exampleText}</a></p>
+        <h1 className='title error-title'>Hmmmmm</h1>
+        <p>Why am I seeing this page?</p>
+        <p className='error-txt'>Please make sure the URL is correctly formatted with a valid <span className='bold'>project id</span> and <span className='bold'>flow id</span>.</p>
+        <p className='error-txt'>For debugging purposes also <span className='bold'>debug=true</span> can be used.</p>
+        <p className='error-blue'>Here's an example: </p>
+        <p className='example' onClick={() => copy(exampleText)}>{exampleText} 
+          {value ? 
+          <CheckIcon className='copy-icon' /> 
+          : 
+          <ContentCopyIcon className='copy-icon'/>
+          }
+         </p>
       </div>
     )
 }
