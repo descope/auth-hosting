a2cd448fdcff4b8eb9f98628c901443f63a1de28
Omer Cohen <omer@descope.com>
2023-06-25 16:35:09 -0700

    feat: project from path (#10)
    * feat: project from path
    
    * fix error
    
    * fix api

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,17 +1,20 @@
-import '../App.css';
+import "../App.css";
 
 function Error() {
-    const exampleText = "https://descope-oidc.com?project=PROJECT_ID&flow=FLOW_ID&debug=true"
-  
-    return (
-      <div className='page error fullscreen'>
-        <h1 className='title'>Hmmmmm</h1>
-        <p>Please make sure the URL is correctly formatted with the right <span>project id</span> and <span>flow id</span>.</p>
-        <p>Here's an example (for descope-oidc.com): </p>
-        <p className='example'><a href={exampleText}>{exampleText}</a></p>
-      </div>
-    )
+  const exampleText = `${window.location.protocol}//${window.location.host}/PROJECT_ID?flow=FLOW_ID&debug=true`;
+
+  return (
+    <div className="page error fullscreen">
+      <h1 className="title">Hmmmmm</h1>
+      <p>
+        Please make sure the URL is correctly formatted with the right <span>project id</span> and <span>flow id</span>.
+      </p>
+      <p>Here's an example: </p>
+      <p className="example">
+        <a href={exampleText}>{exampleText}</a>
+      </p>
+    </div>
+  );
 }
 
-
-export default Error
+export default Error;
