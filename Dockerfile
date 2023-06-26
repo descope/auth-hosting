7dc4821211750f036648acdc10cbe89e6b695b39
Omer Cohen <omer@descope.com>
2023-06-26 16:43:23 +0200

    ci and docker

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+# syntax=docker/dockerfile:1
+ARG NODE_VERSION=18
+
+FROM node:${NODE_VERSION}-alpine as builder
+ENV NODE_ENV=production
+
+WORKDIR /app
+COPY ["package.json", "yarn.lock*", "./"]
+
+RUN yarn install
+COPY . .
+RUN yarn build
+
+FROM nginx:alpine
+
+RUN apk add openssl && \
+    openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt;
+
+RUN cat > /etc/nginx/conf.d/default.conf <<EOF
+server {
+    listen       80;
+    listen       443 ssl http2 default_server;
+    ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+    server_name  localhost;
+
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html;
+        try_files \$uri \$uri/ /index.html;  # this ensures react routing works
+    }
+}
+EOF
+
+EXPOSE 80 443
+WORKDIR /usr/share/nginx/html
+RUN rm -rf ./*
+COPY --from=builder /app/build ./
+
+ENTRYPOINT ["nginx", "-g", "daemon off;"]
--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,30 +1,25 @@
-import '../App.css';
-import { useCopyToClipboard } from 'usehooks-ts';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import CheckIcon from '@mui/icons-material/Check';
+import "../App.css";
+import { useCopyToClipboard } from "usehooks-ts";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CheckIcon from "@mui/icons-material/Check";
 
 const Error = () => {
-	const [value, copy] = useCopyToClipboard();
-	const exampleText = `https://auth.descope.io/PROJECT_ID?flow=FLOW_ID&debug=true`;
-	return (
-		<div className="app-content">
-			<h1 className="error-title">Something went wrong...</h1>
-			<p className="text-body">
-				Please make sure the URL is correctly formatted with a valid{' '}
-				<b>project id</b> and <b>flow id</b>.<br />
-				For debugging purposes also <b>debug=true</b> can be used.
-			</p>
-			<p className="example-title">Here's an example: </p>
-			<p className="example" onClick={() => copy(exampleText)}>
-				{exampleText}
-				{value ? (
-					<CheckIcon className="copy-icon" />
-				) : (
-					<ContentCopyIcon className="copy-icon" />
-				)}
-			</p>
-		</div>
-	);
+  const [value, copy] = useCopyToClipboard();
+  const exampleText = `https://auth.descope.io/PROJECT_ID?flow=FLOW_ID&debug=true`;
+  return (
+    <div className="app-content">
+      <h1 className="error-title">Something went wrong...</h1>
+      <p className="text-body">
+        Please make sure the URL is correctly formatted with a valid <b>project id</b> and <b>flow id</b>.<br />
+        For debugging purposes also <b>debug=true</b> can be used.
+      </p>
+      <p className="example-title">Here's an example: </p>
+      <p className="example" onClick={() => copy(exampleText)}>
+        {exampleText}
+        {value ? <CheckIcon className="copy-icon" /> : <ContentCopyIcon className="copy-icon" />}
+      </p>
+    </div>
+  );
 };
 
 export default Error;
