dee7ac3bff3c938c486798e0b2c8d6c227419f3c
talaharoni <tal@descope.com>
2023-06-26 10:46:22 +0300

    add format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
 import './App.css';
-import { useState } from 'react'
+import { useState } from 'react';
 import { AuthProvider, Descope } from '@descope/react-sdk';
 import Error from './components/Error';
 
 const projectRegex = /^[a-zA-Z0-9]{28}$/;
 
 const App = () => {
-    let pathname = window.location.pathname;
-    if (pathname.startsWith("/")){
-        pathname = pathname.substring(1)
-    }
-    const isValid = projectRegex.test(pathname);
+	let pathname = window.location.pathname;
+	if (pathname.startsWith('/')) {
+		pathname = pathname.substring(1);
+	}
+	const isValid = projectRegex.test(pathname);
 	const urlParams = new URLSearchParams(window.location.search);
 	let projectId = '';
-    if (isValid) {
-        projectId = pathname
-    } else {
-        console.log(`Invalid projectId ${pathname}`)
-    }
+	if (isValid) {
+		projectId = pathname;
+	} else {
+		console.log(`Invalid projectId ${pathname}`);
+	}
 	const flowId = urlParams.get('flow') || 'sign-up-or-in';
 	const debug = urlParams.get('debug') === 'true';
-    const [error, setError] = useState(false)
+	const [error, setError] = useState(false);
 
 	return (
-      <AuthProvider projectId={projectId}>
-        <div className="app">
-          {projectId && flowId && !error? (
-              <div className="descope-container">
-                <Descope
-                  flowId={flowId}
-                  debug={debug}
-                  onError={(e) => setError(true)}
-                />
-              </div>
-          ) : (
-              <Error />
-              )}
-        </div>
-    </AuthProvider>
+		<AuthProvider projectId={projectId}>
+			<div className="app">
+				{projectId && flowId && !error ? (
+					<div className="descope-container">
+						<Descope
+							flowId={flowId}
+							debug={debug}
+							onError={(e) => setError(true)}
+						/>
+					</div>
+				) : (
+					<Error />
+				)}
+			</div>
+		</AuthProvider>
 	);
 };
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
-import Error from "./Error";
+import Error from './Error';
 
 export { Error };
