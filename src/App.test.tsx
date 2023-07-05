8928f44bb11a8455f5442f21d2f724561c56826e
Tal Aharoni <105210819+talaharoni@users.noreply.github.com>
2023-07-05 21:01:47 +0300

    Add test to ci (#19)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("renders without errors", () => {
+    render(<App />);
+  });
+
+  test("displays Error component when projectId is invalid", async () => {
+    Object.assign(navigator, {
+      clipboard: {
+        writeText: () => undefined,
+      },
+    });
+
+    window.location.pathname = "/invalid-project-id";
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("error-component")).toBeInTheDocument();
+    expect(getByTestId("error-copy-component")).toBeInTheDocument();
+    expect(getByTestId("error-copy-icon")).toBeInTheDocument();
+    fireEvent.click(getByTestId("error-copy-component"));
+    await waitFor(() => expect(getByTestId("error-copied-icon")).toBeInTheDocument());
+  });
+
+  test("displays Descope component when projectId is valid", () => {
+    Object.defineProperty(window, "location", {
+      value: {
+        pathname: "/P2Qbs5l8F1kD1g2inbBktiCDummy",
+      },
+      writable: true, // possibility to override
+    });
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("descope-component")).toBeInTheDocument();
+  });
 });
