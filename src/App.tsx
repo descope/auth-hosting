24bdb3f765b70fd03982c0cbb4305a68a85a920d
dorsha <dorsha@descope.com>
2023-07-12 10:29:57 +0300

    Allow passing project id using env var

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,53 @@
 import "./App.css";
 import { useState } from "react";
 import { AuthProvider, Descope } from "@descope/react-sdk";
-import Error from "./components/Welcome";
+import Welcome from "./components/Welcome";
 
 const projectRegex = /^[a-zA-Z0-9]{28}$/;
 
 const App = () => {
-  let pathname = window.location.pathname;
-  if (pathname.startsWith("/")) {
-    pathname = pathname.substring(1);
-  }
-  const isValid = projectRegex.test(pathname);
-  const urlParams = new URLSearchParams(window.location.search);
-  let projectId = "";
-  if (isValid) {
-    projectId = pathname;
-  } else {
-    console.log(`Invalid projectId ${pathname}`);
-  }
-  const flowId = urlParams.get("flow") || "sign-up-or-in";
-  const debug = urlParams.get("debug") === "true";
   const [error, setError] = useState(false);
 
   const baseUrl = process.env.REACT_APP_DESCOPE_BASE_URL || "";
+
+  let projectId = "";
+
+  // first, take project id from env
+  const envProjectId = process.env.DESCOPE_PROJECT_ID;
+  if (envProjectId && projectRegex.test(envProjectId)) {
+    projectId = envProjectId;
+  }
+
+  // if no project id, try to take it from URI
+  if (!projectId) {
+    let pathname = window.location.pathname;
+    if (pathname.startsWith("/")) {
+      pathname = pathname.substring(1);
+      if (projectRegex.test(pathname)) {
+        projectId = pathname;
+      } else {
+        console.log(`Invalid projectId ${pathname}`);
+      }
+    }
+  }
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const flowId = urlParams.get("flow") || "sign-up-or-in";
+  const debug = urlParams.get("debug") === "true";
 
   return (
     <AuthProvider projectId={projectId} baseUrl={baseUrl}>
       <div className="app">
         {projectId && flowId && !error ? (
           <div className="descope-container" data-testid="descope-component">
-            <Descope flowId={flowId} debug={debug} onError={() => setError(true)} />
+            <Descope
+              flowId={flowId}
+              debug={debug}
+              onError={() => setError(true)}
+            />
           </div>
         ) : (
-          <Error />
+          <Welcome />
         )}
       </div>
     </AuthProvider>
