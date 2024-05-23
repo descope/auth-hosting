05cbd72e5e86ba97a1db2b1d3213098cf8412f28
Doron Sharon <dorsha@descope.com>
2024-05-23 19:24:49 +0300

    Add support for sso-config (#461)

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,9 @@ const App = () => {
 	const backgroundColor = urlParams.get('bg') || process.env.DESCOPE_BG_COLOR;
 
 	const isWideContainer =
-		urlParams.get('wide') === 'true' || flowId === 'saml-config';
+		urlParams.get('wide') === 'true' ||
+		flowId === 'saml-config' ||
+		flowId === 'sso-config';
 
 	const containerClasses = clsx('descope-base-container', {
 		'descope-wide-container': isWideContainer,
@@ -50,7 +52,9 @@ const App = () => {
 		flowId,
 		debug,
 		tenant: tenantId,
-		...(flowId === 'saml-config' && { autoFocus: false })
+		...((flowId === 'saml-config' || flowId === 'sso-config') && {
+			autoFocus: false
+		})
 	};
 
 	return (
