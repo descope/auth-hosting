192f65489b6c360f5d6daacaab6fa38a67d48635
Nico <nico@descope.com>
2024-08-15 14:32:46 +0200

    change get favicon fn

--- api/get-favicon.js
+++ api/get-favicon.js
@@ -2,32 +2,17 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
 	const { url } = req.query;
-
-	// URL for default favicon
-	const defaultFaviconUrl = 'https://app.descope.com/descope_favicon_24X24.ico';
+	const defaultFaviconUrl = process.env.DEFAULT_FAVICON_URL;
 
 	try {
-		// Fetch the custom favicon
-		// to-do: Leave it to Nico to do his magic here
-		const response = await fetch(`${url}/favicon.ico`);
+		const response = await fetch(url);
 
 		if (response.ok) {
-			// If found, pipe the result
-			const faviconBuffer = await response.buffer();
-			res.setHeader('Content-Type', 'image/x-icon');
-			res.send(faviconBuffer);
+			return res.status(200).json({ faviconUrl: url });
 		} else {
-			// If not found, fallback to default
-			const defaultResponse = await fetch(defaultFaviconUrl);
-			const defaultFaviconBuffer = await defaultResponse.buffer();
-			res.setHeader('Content-Type', 'image/x-icon');
-			res.send(defaultFaviconBuffer);
+			return res.status(200).json({ faviconUrl: defaultFaviconUrl });
 		}
 	} catch (error) {
-		// In case of an error, return default favicon
-		const defaultResponse = await fetch(defaultFaviconUrl);
-		const defaultFaviconBuffer = await defaultResponse.buffer();
-		res.setHeader('Content-Type', 'image/x-icon');
-		res.send(defaultFaviconBuffer);
+		return res.status(200).json({ faviconUrl: defaultFaviconUrl });
 	}
 }
