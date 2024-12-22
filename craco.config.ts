1d7b9af7deccfe25e4e3025dfa5e3491b22f6ed9
tomski747 <tomski747@users.noreply.github.com>
2024-12-22 09:55:00 +0200

    Add version route

--- craco.config.ts
+++ craco.config.ts
@@ -10,7 +10,20 @@ export default <CracoConfig>{
 				new CycloneDxWebpackPlugin({
 					includeWellknown: false,
 					outputLocation: '../.bom'
-				})
+				}),
+				{
+					apply: (compiler) => {
+						compiler.hooks.afterEmit.tap('ReplaceVersionPlugin', () => {
+							const fs = require('fs');
+							const gitSha =
+								process.env.VERCEL_GIT_COMMIT_SHA ??
+								process.env.GITHUB_SHA ??
+								process.env.GIT_SHA ??
+								'local';
+							fs.writeFileSync('public/version', gitSha);
+						});
+					}
+				}
 			]
 		}
 	}
