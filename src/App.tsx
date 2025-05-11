a3d0262315f7e0d02dff3a72d10e7eef28be2991
Tom Kaminski <tom@descope.com>
2025-05-11 13:22:58 +0300

    Remove static favicon (#839)
    * Hide default favicon
    
    * Remove svg

--- src/App.tsx
+++ src/App.tsx
@@ -84,11 +84,6 @@ const App = () => {
 			faviconUrlTemplate
 		});
 
-		if (!ssoAppId) {
-			logger.log('Missing ssoAppId skipping favicon update');
-			return;
-		}
-
 		if (!defaultFaviconUrl) {
 			logger.log('Missing defaultFaviconUrl skipping favicon update');
 			return;
@@ -106,10 +101,13 @@ const App = () => {
 		}
 
 		logger.log('URL is secure, fetching favicon...');
-		const existingFaviconUrl = await getFaviconUrl(
-			faviconUrl,
-			defaultFaviconUrl
-		);
+
+		// If ssoAppId is not provided, fallback to using the default favicon URL.
+		let existingFaviconUrl = defaultFaviconUrl;
+		if (ssoAppId) {
+			logger.log('Checking custom favicon for ssoAppId:', ssoAppId);
+			existingFaviconUrl = await getFaviconUrl(faviconUrl, defaultFaviconUrl);
+		}
 
 		let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
 		if (!link) {
