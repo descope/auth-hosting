a89f19cb352ffce491b82f075dc129da2efbe520
Asaf Rosentswaig <asaf@daylight.ai>
2026-01-18 14:59:33 +0200

    Use default app id when not in url

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import { logger } from './utils/logger';
 const projectRegex = /^P([a-zA-Z0-9]{27}|[a-zA-Z0-9]{31})$/;
 const ssoAppRegex = /^[a-zA-Z0-9\-_]{1,30}$/;
 
+const DEFAULT_SSO_APP_ID = 'descope-default-oidc';
+
 const isFaviconUrlSecure = (url: string) => {
 	try {
 		const parsedUrl = new URL(url);
@@ -113,7 +115,7 @@ const App = () => {
 		[]
 	);
 
-	let ssoAppId = urlParams.get('sso_app_id') || '';
+	let ssoAppId = urlParams.get('sso_app_id') || DEFAULT_SSO_APP_ID;
 	ssoAppId = ssoAppRegex.exec(ssoAppId)?.[0] || '';
 
 	// Memoize updateFavicon with useCallback
